9e8925bfb50c97ee1efc41dc7e0b20489cbd7710
Derick <derick.derick@rakuten.com>
2024-07-10 16:33:51 +0800

    Update 7.0.0 beta 2

--- Package.swift
+++ Package.swift
@@ -3,17 +3,39 @@
 
 import PackageDescription
 
+// let package = Package(
+//     name: "RakutenRewardNativeSDK",
+//     platforms: [.iOS(.v13)],
+//     products: [
+//         .library(
+//             name: "RakutenRewardNativeSDK",
+//             targets: ["RakutenRewardNativeSDK"]),
+//     ],
+//     targets: [
+//         .binaryTarget(
+//             name: "RakutenRewardNativeSDK",
+//             url: "https://github.com/rakuten-ads/Rakuten-Reward-Native-iOS/releases/download/7.0.0/RakutenRewardNativeSDKCombined.zip",
+//             checksum: "81cd0f24b2ff61ae4337ac12d2721cf95c5a074731dc4eaf9702a4a54ecada08"
+//         ),
+//     ]
+// )
+
 let package = Package(
     name: "RakutenRewardNativeSDK",
     platforms: [.iOS(.v13)],
     products: [
         .library(
             name: "RakutenRewardNativeSDK",
-            targets: ["RakutenRewardNativeSDK"]),
+            targets: ["RakutenRewardNativeSDK", "ScreenSDKCore"]),
     ],
     targets: [
         .binaryTarget(
             name: "RakutenRewardNativeSDK",
+            url: "https://github.com/rakuten-ads/Rakuten-Reward-Native-iOS/releases/download/7.0.0/RakutenRewardNativeSDKCombined.zip",
+            checksum: "81cd0f24b2ff61ae4337ac12d2721cf95c5a074731dc4eaf9702a4a54ecada08"
+        ),
+        .binaryTarget(
+            name: "ScreenSDKCore",
             url: "https://github.com/rakuten-ads/Rakuten-Reward-Native-iOS/releases/download/7.0.0/RakutenRewardNativeSDKCombined.zip",
             checksum: "81cd0f24b2ff61ae4337ac12d2721cf95c5a074731dc4eaf9702a4a54ecada08"
         ),
