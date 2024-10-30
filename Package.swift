bac12f6fa9c8420a5313b873189fe757bd31a259
Derick <derick.derick@rakuten.com>
2024-10-30 12:21:44 +0800

    Update v8.0.0

--- Package.swift
+++ Package.swift
@@ -15,12 +15,12 @@ let package = Package(
         .binaryTarget(
             name: "RakutenRewardNativeSDK",
             url: "https://github.com/rakuten-ads/Rakuten-Reward-Native-iOS/releases/download/8.1.0/RakutenRewardNativeSDK.xcframework.zip",
-            checksum: "5658eddbb62e455f6a64b30f20e2ad133ba1ca103562dbb0649168e97fbf1347"
+            checksum: "4205b32b312de77ff7d97c8446d04e15456a9050d56a881b6c2301ef6f29d958"
         ),
         .binaryTarget(
             name: "ScreenSDKCore",
             url: "https://github.com/rakuten-ads/Rakuten-Reward-Native-iOS/releases/download/8.1.0/ScreenSDKCore.xcframework.zip",
-            checksum: "ec5abd2edc20ce36694d7749f8f21cda332f81f2c9d2bc4205b16bbee4d96e0e"
+            checksum: "c9b20db7a86038808d8595dae48ee5a4f0637f1a7772be2443ba1d41d09bbe63"
         ),
     ]
 )
